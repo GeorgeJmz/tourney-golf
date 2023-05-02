9895df7eb26cb8a9d18eafcdd0cff96253b976a3
jeckox <jeckox@gmail.com>
2023-05-01 19:41:29 -0600

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App (): JSX.Element {
+  console.log('ok')
+  return (
+    <div className="App">
+      <h1>
+        Tourney Golf
+      </h1>
+    </div>
+  )
+}
+
+export default App
