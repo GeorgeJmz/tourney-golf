82bef2863b9a32cfac222c1496149d7ca90cb2db
jeckox <jeckox@gmail.com>
2024-04-16 17:24:54 -0600

    TEEBOX LEAGUE - Add Post Score Message"

--- src/views/Play/components/ScoreBeforeLeave.tsx
+++ src/views/Play/components/ScoreBeforeLeave.tsx
@@ -160,6 +160,26 @@ export const ScoreBeforeLeave: React.FC<IScoreBeforeLeaveProps> = ({
           sx={{
             display: "flex",
             justifyContent: "center",
+            marginTop: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Leave a message"
+            variant="outlined"
+            placeholder="(Optional)"
+            fullWidth
+            value={message}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
             gap: "12px",
             marginTop: "10px",
           }}
