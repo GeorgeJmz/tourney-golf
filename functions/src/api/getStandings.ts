34e73770d5b9f86fafa037044cf12f85fa84681b
jeckox <jeckox@gmail.com>
2025-03-21 15:15:42 -0600

    Add Standings logic

--- functions/src/api/getStandings.ts
+++ functions/src/api/getStandings.ts
@@ -17,27 +17,24 @@ export const getStandings = async (req: RequestGetStandings, res: Response) => {
   }
 
   try {
-    const tournamentsSnapshot = await db
-      .collection("tournament")
-      .where("id", "==", leagueId)
-      .get();
+    const [tournamentsSnapshot, playersSnapshot] = await Promise.all([
+      db.collection("tournament").where("id", "==", leagueId).get(),
+      db.collection("player").where("tournamentId", "==", leagueId).get(),
+    ]);
 
     if (tournamentsSnapshot.empty) {
       return res.status(404).json({ error: "Tournament not found" });
     }
 
-    const playersSnapshot = await db
-      .collection("player")
-      .where("tournamentId", "==", leagueId)
-      .get();
     const currentLeague = tournamentsSnapshot.docs[0].data() as ITournament;
-
-    const tournamentType = currentLeague.tournamentType;
-    const playType = currentLeague.playType;
-    const pointsPerTie = currentLeague.pointsPerTie;
-    const pointsPerWin = currentLeague.pointsPerWin;
-    const pointsPerTieMedal = currentLeague.pointsPerTieMedal;
-    const pointsPerWinMedal = currentLeague.pointsPerWinMedal;
+    const {
+      tournamentType,
+      playType,
+      pointsPerTie,
+      pointsPerWin,
+      pointsPerTieMedal,
+      pointsPerWinMedal,
+    } = currentLeague;
 
     const isLTMATCH =
       tournamentType === "leagueteamplay" && playType === "matchplaystableford";
@@ -45,18 +42,12 @@ export const getStandings = async (req: RequestGetStandings, res: Response) => {
       tournamentType === "leagueteamplay" && playType === "medalplaystableford";
     const isLMATCH = tournamentType === "league" && playType === "matchPlay";
     const isLMEDAL = tournamentType === "league" && playType === "strokePlay";
-
     const isDogFight = tournamentType === "dogfight";
 
-    const players: ITournamentPlayer[] = [];
-    playersSnapshot.forEach((doc) => {
-      const player = doc.data() as ITournamentPlayer;
-      players.push({
-        ...doc.data(),
-        id: doc.id,
-      } as unknown as ITournamentPlayer);
-      logger.info("player", { player });
-    });
+    const players: ITournamentPlayer[] = playersSnapshot.docs.map((doc) => ({
+      ...(doc.data() as ITournamentPlayer),
+      id: doc.id,
+    }));
 
     const emails = players.map((player) => player.email);
 
@@ -64,19 +55,18 @@ export const getStandings = async (req: RequestGetStandings, res: Response) => {
     const chunkSize = 30;
 
     for (let i = 0; i < emails.length; i += chunkSize) {
-      if (i + chunkSize > emails.length) {
-        const chunk = emails.slice(i);
-        const usersSnapshot = await db
-          .collection("users")
-          .where("email", "in", chunk)
-          .get();
-        usersSnapshot.forEach((doc) => {
-          users.push(doc.data() as IUser);
-        });
-      }
+      const chunk = emails.slice(i, i + chunkSize);
+      const usersSnapshot = await db
+        .collection("users")
+        .where("email", "in", chunk)
+        .get();
+      usersSnapshot.forEach((doc) => {
+        users.push(doc.data() as IUser);
+      });
     }
+
     const nameMap = new Map(
-      users.map((player) => [player.email, player.name + " " + player.lastName])
+      users.map((user) => [user.email, `${user.name} ${user.lastName}`])
     );
 
     const playersWithNames = players.map((player) => ({
@@ -84,166 +74,103 @@ export const getStandings = async (req: RequestGetStandings, res: Response) => {
       name: nameMap.get(player.email) || player.name,
     }));
 
+    const calculatePlayerStats = async (player: ITournamentPlayer) => {
+      const getAverage = (values: number[]) => {
+        const sum = values.reduce((acc, curr) => acc + curr, 0);
+        return sum > 0 ? (sum / values.length).toFixed(1) : "0";
+      };
+
+      const calculatePoints = (
+        pointsArray: number[],
+        winPoint: number,
+        tiePoint: number
+      ) => {
+        const wins = pointsArray.filter((point) => point === winPoint).length;
+        const draws = pointsArray.filter((point) => point === tiePoint).length;
+        const losses = pointsArray.filter((point) => point === 0).length;
+        const total = pointsArray.reduce((acc, curr) => acc + curr, 0);
+        return { wins, draws, losses, total };
+      };
+
+      const matchPoints = calculatePoints(
+        player.pointsMatch,
+        pointsPerWin,
+        pointsPerTie
+      );
+      const strokePoints = calculatePoints(
+        player.pointsStroke,
+        pointsPerWinMedal,
+        pointsPerTieMedal
+      );
+
+      const bonusPoints = player.bonusPoints
+        ? player.bonusPoints.reduce((acc, curr) => acc + curr, 0)
+        : 0;
+
+      const totalPoints =
+        isLTMATCH || isLMATCH
+          ? matchPoints.total + bonusPoints
+          : isLTMEDAL || isLMEDAL
+          ? strokePoints.total + bonusPoints
+          : matchPoints.total + strokePoints.total + bonusPoints;
+
+      return {
+        id: player.id,
+        tourneyName: player.name,
+        matchesPlayed:
+          playType !== "matchstrokePlay"
+            ? player.opponent.length
+            : player.opponent.length * 2,
+        wins: isLTMATCH || isLMATCH ? matchPoints.wins : strokePoints.wins,
+        draws: isLTMATCH || isLMATCH ? matchPoints.draws : strokePoints.draws,
+        losses:
+          isLTMATCH || isLMATCH ? matchPoints.losses : strokePoints.losses,
+        bonusPoints: bonusPoints,
+        matchPoints: matchPoints.total,
+        medalPoints: strokePoints.total,
+        totalPoints: totalPoints,
+        grossAverage: getAverage(
+          player.gross.map((value) => parseInt(value.toString()))
+        ),
+        handicapAverage: getAverage(
+          player.handicap.map((value) => parseInt(value.toString()))
+        ),
+        netAverage: getAverage(
+          player.net.map((value) => parseInt(value.toString()))
+        ),
+        teamPoints: player.pointsTeam.reduce((acc, curr) => acc + curr, 0),
+        conference: player.conference,
+        group: player.group,
+      };
+    };
+
     const newPlayers = await Promise.all(
-      playersWithNames.map(async (player) => {
-        const getAverage = (values: Array<number>) => {
-          const average = values.reduce((acc, curr) => acc + curr, 0);
-          return average > 0 ? (average / values.length).toFixed(1) : "0";
-        };
-
-        const getWins = () => {
-          if (isLTMATCH || isLMATCH) {
-            return player.pointsMatch.reduce(
-              (acc, curr) => (curr === pointsPerWin ? acc + 1 : acc),
-              0
-            );
-          }
-          if (isLTMEDAL || isLMEDAL) {
-            return player.pointsStroke.reduce(
-              (acc, curr) => (curr === pointsPerWinMedal ? acc + 1 : acc),
-              0
-            );
-          }
-          return (
-            player.pointsMatch.reduce(
-              (acc, curr) => (curr === pointsPerWin ? acc + 1 : acc),
-              0
-            ) +
-            player.pointsStroke.reduce(
-              (acc, curr) => (curr === pointsPerWinMedal ? acc + 1 : acc),
-              0
-            )
-          );
-        };
-
-        const getDraws = () => {
-          if (isLTMATCH || isLMATCH) {
-            return player.pointsMatch.reduce(
-              (acc, curr) => (curr === pointsPerTie ? acc + 1 : acc),
-              0
-            );
-          }
-          if (isLTMEDAL || isLMEDAL) {
-            return player.pointsStroke.reduce(
-              (acc, curr) => (curr === pointsPerTieMedal ? acc + 1 : acc),
-              0
-            );
-          }
-          return (
-            player.pointsMatch.reduce(
-              (acc, curr) => (curr === pointsPerTie ? acc + 1 : acc),
-              0
-            ) +
-            player.pointsStroke.reduce(
-              (acc, curr) => (curr === pointsPerTieMedal ? acc + 1 : acc),
-              0
-            )
-          );
-        };
-
-        const getLoss = () => {
-          if (isLTMATCH || isLMATCH) {
-            return player.pointsMatch.reduce(
-              (acc, curr) => (curr === 0 ? acc + 1 : acc),
-              0
-            );
-          }
-          if (isLTMEDAL || isLMEDAL) {
-            return player.pointsStroke.reduce(
-              (acc, curr) => (curr === 0 ? acc + 1 : acc),
-              0
-            );
-          }
-          return (
-            player.pointsMatch.reduce(
-              (acc, curr) => (curr === 0 ? acc + 1 : acc),
-              0
-            ) +
-            player.pointsStroke.reduce(
-              (acc, curr) => (curr === 0 ? acc + 1 : acc),
-              0
-            )
-          );
-        };
-
-        const getBonusPoints = () =>
-          player.bonusPoints
-            ? player.bonusPoints.reduce((acc, curr) => acc + curr, 0)
-            : 0;
-
-        const getTotalPoints = () => {
-          if (isLTMATCH || isLMATCH) {
-            return (
-              player.pointsMatch.reduce((acc, curr) => acc + curr, 0) +
-              getBonusPoints()
-            );
-          }
-          if (isLTMEDAL || isLMEDAL) {
-            return (
-              player.pointsStroke.reduce((acc, curr) => acc + curr, 0) +
-              getBonusPoints()
-            );
-          }
-          return (
-            player.pointsMatch.reduce((acc, curr) => acc + curr, 0) +
-            player.pointsStroke.reduce((acc, curr) => acc + curr, 0) +
-            getBonusPoints()
-          );
-        };
-
-        return {
-          id: player.id,
-          position: 0, // Add position property
-          tourneyName: player.name, // Add tourneyName property
-          matchesPlayed:
-            playType !== "matchstrokePlay"
-              ? player.opponent.length
-              : player.opponent.length * 2,
-          wins: getWins(),
-          draws: getDraws(),
-          losses: getLoss(),
-          bonusPoints: getBonusPoints(),
-          matchPoints: player.pointsMatch.reduce((acc, curr) => acc + curr, 0),
-          medalPoints: player.pointsStroke.reduce((acc, curr) => acc + curr, 0),
-          totalPoints: getTotalPoints(),
-          grossAverage: getAverage(
-            player.gross.map((value) => parseInt(value.toString()))
-          ),
-          handicapAverage: getAverage(
-            player.handicap.map((value) => parseInt(value.toString()))
-          ),
-          netAverage: getAverage(
-            player.net.map((value) => parseInt(value.toString()))
-          ),
-          teamPoints: player.pointsTeam.reduce((acc, curr) => acc + curr, 0),
-          conference: player.conference,
-          group: player.group,
-        };
-      })
+      playersWithNames.map(calculatePlayerStats)
     );
 
-    const result = !isDogFight
+    const sortedPlayers = !isDogFight
       ? newPlayers.sort((a, b) => b.totalPoints - a.totalPoints)
       : newPlayers
           .sort((a, b) => {
-            if (parseInt(a.netAverage) > 0 && parseInt(b.netAverage) > 0) {
-              return parseInt(b.netAverage) - parseInt(a.netAverage);
-            } else if (
-              parseInt(a.netAverage) <= 0 &&
-              parseInt(b.netAverage) <= 0
-            ) {
-              return 0;
-            } else {
-              return parseInt(a.netAverage) - parseInt(b.netAverage);
+            const aNet = parseInt(a.netAverage);
+            const bNet = parseInt(b.netAverage);
+            if (aNet > 0 && bNet > 0) {
+              return bNet - aNet;
             }
+            if (aNet <= 0 && bNet <= 0) {
+              return 0;
+            }
+            return aNet - bNet;
           })
           .reverse();
 
-    const standingsPositions = result.slice(0, 3).map((player, index) => ({
-      name: player.tourneyName,
-      points: isDogFight ? player.netAverage : player.totalPoints,
-      position: index + 1,
-    }));
+    const standingsPositions = sortedPlayers
+      .slice(0, 3)
+      .map((player, index) => ({
+        name: player.tourneyName,
+        points: isDogFight ? player.netAverage : player.totalPoints,
+        position: index + 1,
+      }));
 
     return res.status(200).json({
       status: "success",
