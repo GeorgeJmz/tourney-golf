f9e3c6fef169901e42578de066d3f789873b6395
jeckox <jeckox@gmail.com>
2025-04-16 13:41:01 -0600

    Add Dogfight getOpponents

--- functions/src/types/Match.ts
+++ functions/src/types/Match.ts
@@ -9,4 +9,19 @@ export interface IMatchResults {
   isWinnerMedalPlay: boolean;
   medalPoints?: number;
   matchPoints?: number;
+  bonusPoints?: number;
+}
+export interface IMatch {
+  author: string;
+  course: string;
+  courseDisplayName: string;
+  teeBox: string;
+  teeBoxDisplayName: string;
+  scoresId: Array<string>;
+  winner: string;
+  date: Array<string>;
+  tournamentId: string;
+  matchResults: Array<IMatchResults>;
+  id?: string;
+  round?: number;
 }
