528c20609c328471e81db757abb438fbe9614b49
jeckox <jeckox@gmail.com>
2025-06-13 05:55:34 -0600

    Remove Unnecessary Dependencies

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { Link, Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../hooks/useUserContext";
-import { Button, Stack, Typography, Paper, Container } from "@mui/material";
 import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
 import { AppDownload } from "./components/AppDownload";
